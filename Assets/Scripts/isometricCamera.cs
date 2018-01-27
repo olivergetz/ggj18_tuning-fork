aecc2c83c3e1bc5cfbec9ea61dff58485afd1bc4
Oliver Getz Rodahl <olivergetz@gmail.com>
2018-01-27 15:15:40 -0500

    Player Movement

---
+++ Assets/Scripts/isometricCamera.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isometricCamera : MonoBehaviour {
+
+	public Vector3 posOffset = new Vector3 (0, 3, -8);
+	public Quaternion rotOffset;
+	public GameObject playerObject;
+
+	float rotateVel = 0;
+
+	Vector3 destination = Vector3.zero; //This is where we store the target destination of our camera.
+	Vector3 distance;
+
+	Transform target;
+
+	[Range(0.01f,1.0f)]
+	public float smooth = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		//Initialize Camera Target
+		if (playerObject != null)
+
+			target = playerObject.GetComponent<Transform> ();
+
+		 else 
+
+			Debug.LogError ("Your camera needs a target.");
+
+		distance = transform.position - target.position;
+		
+	}
+	
+	// Update is called regularly.
+	void FixedUpdate () {
+
+		//Update Camera Position and Rotation
+//		lookAtTarget ();
+		moveToTarget ();
+		
+	}
+
+	void moveToTarget(){
+
+		destination = playerObject.transform.position + distance + posOffset;
+		transform.position = Vector3.Slerp (transform.position, destination, smooth);
+
+	}
+
+	void lookAtTarget(){
+
+		float eulerYangle = Mathf.SmoothDampAngle (transform.rotation.y, target.rotation.y, ref rotateVel, 0.09f);
+		transform.rotation = Quaternion.Euler (transform.rotation.x, eulerYangle, transform.rotation.z);
+
+	}
+
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	[System.Serializable] //Make settings adjustable in inspector.
+	public class MoveSettings{
+
+		public float movementSpeed = 10f;
+
+	}
+
+	[System.Serializable]
+	public class InputSettings{
+
+		public string UPDOWN_AXIS = "Vertical";
+		public string LR_AXIS = "Horizontal";
+
+	}
+
+	//References to classes
+	public MoveSettings moveSettings = new MoveSettings ();
+	public InputSettings inputSettings = new InputSettings ();
+
+	Rigidbody rBody;
+
+	private Vector3 velocity = Vector3.zero; //Store player velocities here.
+	Vector3 directionOffset = Vector3.zero;
+	Quaternion rotationOffset = Quaternion.Euler(0,45,0);
+
+	//Variable Definitions
+	float upDownInput;
+	float LRInput;
+
+	// Use this for initialization
+	void Start () {
+
+		//Rotate the object by 45 degrees.
+//		transform.rotation = Quaternion.Euler(0,45,0);
+
+		//Check if we have a rigidbody, else throw an error.
+		if (GetComponent<Rigidbody>())
+			
+			rBody = GetComponent<Rigidbody> ();
+
+		else
+
+			Debug.LogError("The player object needs a rigidbody.");
+
+	}
+
+	void FixedUpdate(){ //Used to add force to a rigidbody every fixed frame, because it is regular.
+
+		//Move based on camera view
+		Vector3 rightLeft = Camera.main.transform.right * moveSettings.movementSpeed * LRInput;
+		Vector3 upDown = Camera.main.transform.forward * moveSettings.movementSpeed * upDownInput;
+
+		velocity = rightLeft + upDown;
+
+		//Don't move on y axis.
+		velocity.y = 0f;
+
+		rBody.velocity = velocity;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetInput ();
+		
+	}
+
+	//Gather input data
+	void GetInput(){
+
+		upDownInput = Input.GetAxis (inputSettings.UPDOWN_AXIS); //Stores a float from -1 to 1.
+		LRInput = Input.GetAxis (inputSettings.LR_AXIS); //Stores a float from -1 to 1.
+
+		directionOffset.z = upDownInput;
+		directionOffset.x = LRInput;
+
+
+	}
+
+	void Rotate(){
+
+		//Use velocity to calculate rotation.
+
+
+	}
+
+}
